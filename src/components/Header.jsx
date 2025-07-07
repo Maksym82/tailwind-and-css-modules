03800a1c3d13abde067b0f1aabd298d32ebb276f
Maksym <112901851+Maksym82@users.noreply.github.com>
2025-07-07 15:09:07 +0300

    Add tailwind

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,7 @@ import { NavLink } from "react-router-dom";
 function Header() {
   return (
     <>
-      <h1>Header</h1>
-      {/* <a href="http://localhost:5173/about">about</a> */}
+      <h1 className="flex mb-5 text-4xl text-red-600 ">Header</h1>
       <NavLink to={"/"}>Home</NavLink>
       <NavLink to={"/about"}>About</NavLink>
       <NavLink to={"/cart"}>Cart</NavLink>
