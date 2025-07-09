e010093f62f2b2ab96043f156665961b7979363e
Maksym <112901851+Maksym82@users.noreply.github.com>
2025-07-09 12:34:05 +0300

    Add styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,14 +8,14 @@ function Home() {
 
   return (
     <div className="py-10">
-      <h1 className="mb-8 text-2xl font-semibold text-center">Categories</h1>
+      <h1 className="mb-8 text-2xl font-semibold text-center text-primary">Categories</h1>
       <ul className="grid grid-cols-3 gap-4 px-5">
         {categories.map((category) => (
           <li key={category.id}>
             <Link className="relative flex flex-col items-center justify-center" to={`/category/${category.name}`}>
               <span className="absolute z-10 text-xl font-semibold text-white">{category.name}</span>
               <img className="rounded-md" src={category.img} alt={category.name} />
-              <div className="absolute inset-0 rounded-md bg-gray-950 opacity-40"></div>
+              <div className="absolute inset-0 rounded-md bg-gray-950 opacity-40 bg-custom-gradient"></div>
             </Link>
           </li>
         ))}
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,14 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      // backgroundImage: {
-      //   "custom-gradient":
-      //     "linear-gradient(0deg, rgba(70,23,84,1) 0%, rgba(74,157,32,0.9449406895068807) 33%, rgba(84,23,26,0.4555675185079726) 68%, rgba(57,29,51,0.6811792216169725) 87%, rgba(23,37,84,0.9839315151949541) 100%);",
-      // },
-      // colors: {
-      //   primary: "#3F32F6",
-      //   secondaty: "#1E3A8A",
-      // },
+      backgroundImage: {
+        "custom-gradient":
+          "linear-gradient(0deg, rgba(70,23,84,1) 0%, rgba(74,157,32,0.9449406895068807) 33%, rgba(84,23,26,0.4555675185079726) 68%, rgba(57,29,51,0.6811792216169725) 87%, rgba(23,37,84,0.9839315151949541) 100%);",
+      },
+      colors: {
+        primary: "#3F32F6",
+        secondary: "#1E3A8A",
+      },
       // fontFamily: {
       //   poppins: "poppins, sans-serif",
       // },
