c2be3d5aee4b454716b47cbce55cb93cc3f2358d
Maksym <112901851+Maksym82@users.noreply.github.com>
2025-07-10 11:04:12 +0300

    Add styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,13 +7,13 @@ function Home() {
   console.log(location);
 
   return (
-    <div className="py-10">
+    <div className="py-10 sm:py-16 md:py-20">
       <h1 className="mb-8 text-2xl font-semibold text-center text-primary">Categories</h1>
       <ul className="grid grid-cols-3 gap-4 px-5">
         {categories.map((category) => (
           <li key={category.id}>
-            <Link className="relative flex flex-col items-center justify-center" to={`/category/${category.name}`}>
-              <span className="absolute z-10 text-xl font-semibold text-white">{category.name}</span>
+            <Link className="relative flex flex-col items-center justify-center group" to={`/category/${category.name}`}>
+              <span className="absolute z-10 text-xl font-semibold text-white transition-all group-hover:text-red-600 group-hover:text-2xl">{category.name}</span>
               <img className="rounded-md" src={category.img} alt={category.name} />
               <div className="absolute inset-0 rounded-md bg-gray-950 opacity-40 bg-custom-gradient"></div>
             </Link>
